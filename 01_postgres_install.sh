69c9655583e4febd867fec3042ec2d7c8bf25eda
jvinc86 <jvincent.camacho@gmail.com>
2022-09-08 15:14:45 +0200

    Mejoras para descargar el keyring qlo de postgresql

--- 01_postgres_install.sh
+++ 01_postgres_install.sh
@@ -10,8 +10,12 @@ sudo apt update -y && sudo apt upgrade -y
 sudo apt install openjdk-11-jdk -y
 
 # ---------------- INSTALACION POSTGRESQL 14 ----------------------------
+sudo apt -y install gnupg2 wget ca-certificates lsb-release software-properties-common
+
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+# wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+curl -sS https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/apt.postgresql.org.gpg >/dev/null
+
 sudo apt update -y
 sudo apt install -y postgresql
 
@@ -23,7 +27,7 @@ usuario_psql=postgres; contrasena_psql=123;
 echo "$usuario_psql:$contrasena_psql" | sudo chpasswd
 echo "postgres ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
 
-# Crear BD para sonarqube
+# ------------- Crear BD para sonarqube --------------
 sudo -u postgres -H -- psql -c "CREATE USER sonar WITH ENCRYPTED PASSWORD '123';"
 sudo -u postgres -H -- psql -c "CREATE DATABASE sonarqube OWNER sonar;"
 sudo -u postgres -H -- psql -c "GRANT ALL PRIVILEGES ON DATABASE sonarqube TO sonar;"
