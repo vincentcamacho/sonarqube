39b66423e8148a46c36795786dfb2c588d1a3916
jvinc86 <jvincent.camacho@gmail.com>
2022-09-08 13:02:20 +0200

    Update 01_postgres_install.sh

--- 01_postgres_install.sh
+++ 01_postgres_install.sh
@@ -10,12 +10,14 @@ sudo apt update -y && sudo apt upgrade -y
 sudo apt install openjdk-11-jdk -y
 
 # ---------------- INSTALACION POSTGRESQL 14 ----------------------------
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
-# wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
-wget -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt update -y
-sudo apt install postgresql -y
-sudo systemctl start postgresql; sudo systemctl enable postgresql;sudo systemctl status postgresql
+sudo apt install -y postgresql
+
+sudo systemctl start postgresql
+sudo systemctl enable postgresql
+sudo systemctl status postgresql
 
 usuario_psql=postgres; contrasena_psql=123;
 echo "$usuario_psql:$contrasena_psql" | sudo chpasswd
